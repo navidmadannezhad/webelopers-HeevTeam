ae2074edf07da2db06ff6296aa630c098234998f
navid.mnzh <navidproject283@gmail.com>
2021-03-04 18:22:35 +0330

    feature 3 Done

--- ecommerce/views.py
+++ ecommerce/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 from django.http import HttpResponse
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, auth
 from django.contrib.auth.hashers import make_password
 
 def returnHomePage(request):
@@ -13,7 +13,7 @@ def returnRegisterPage(request):
 def registerUser(request):
     if request.method == 'POST':
         createUser = User.objects.create(first_name= request.POST['first_name'],last_name= request.POST['last_name'], email= request.POST['email'], password= make_password(request.POST['password1']), username= request.POST['username'])
-
+        return render(request, 'login.html')
 
     return render(request, 'register.html')
 
@@ -23,9 +23,9 @@ def returnLoginPage(request):
 def loginUser(request):
     username = request.POST['username']
     password = request.POST['password']
-    user = authenticate(username= username, password = password)
+    user = auth.authenticate(username= username, password = password)
     if user:
-        login(request, user)
+        auth.login(request, user)
         return redirect('/')
     errors = 'نام کاربری یا رمز عبور اشتباه می باشد'
-    return render(request, 'login.html', { 'errors': errors })
+    return render(request, 'login.html', {'errors': errors})
