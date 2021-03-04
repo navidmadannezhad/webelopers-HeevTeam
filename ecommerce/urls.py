e116f44ce88abab0ca0b903a99b0cb00ca726847
navid.mnzh <navidproject283@gmail.com>
2021-03-04 13:56:59 +0330

    feature 2 Done!

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.returnHomePage, name="homePage")
+    path('', views.returnHomePage, name="homePage"),
+    path('register/', views.returnRegisterPage, name="registerPage"),
+    path('register-user/', views.registerUser, name="registerUser")
 ]
--- ecommerce/views.py
+++ ecommerce/views.py
@@ -1,4 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 def returnHomePage(request):
     return render(request, 'home.html')
+
+def returnRegisterPage(request):
+    return render(request, 'register.html')
+
+def registerUser(request):
+    if request.method == 'POST':
+        createUser = User.objects.create(first_name= request.POST['first_name'],last_name= request.POST['last_name'], email= request.POST['email'], password= request.POST['password1'], username= request.POST['username'])
+
+
+    return render(request, 'register.html')
